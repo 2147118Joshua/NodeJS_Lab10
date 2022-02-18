ef6cfa8657eb27f5d1d7bdc7b6ba25598195a82e
Joshua Stalin <95157467+2147118Joshua@users.noreply.github.com>
2022-02-18 11:28:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const http = require('http');
+//const bcrypt = require('bcrypt');
+const path = require("path");
+const bodyParser = require('body-parser');
+const users = require('./data').userDB;
+const fs = require('fs');
+const vehicles = require("./vehicles.json");
+
+
+const app = express();
+var load = () => JSON.parse(fs.readFileSync('vehicles.json'));
+app.use(express.urlencoded({ extended: true }));
+
+
+const server = http.createServer(app);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'./public')));
+
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+
+
+
+app.post('/register', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (!foundUser) {
+    
+            let hashPassword = await bcrypt.hash(req.body.password, 10);
+    
+            let newUser = {
+                id: Date.now(),
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+            };
+            users.push(newUser);
+            console.log('User list', users);
+             vehicles['Vehicles'].push(newUser);
+    
+    fs.writeFile("vehicles.json", JSON.stringify(vehicles,null,4), err => { 
+
+    if(err){
+        console.error(err);
+    }
+    console.log(vehicles)
+    console.log("Done writing"); // Success
+    });
+
+            res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
+        } else {
+            res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+/*
+app.post('/login', async (req, res) => {
+    try{
+        let foundUser = users.find((data) => req.body.email === data.email);
+        if (foundUser) {
+    
+            let submittedPass = req.body.password; 
+            let storedPass = foundUser.password; 
+    
+            const passwordMatch = await bcrypt.compare(submittedPass, storedPass);
+            if (passwordMatch) {
+                let usrname = foundUser.username;
+                res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+            } else {
+                res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
+            }
+        }
+        else {
+    
+            let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+            await bcrypt.compare(req.body.password, fakePass);
+    
+            res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align='center'><a href='./login.html'>login again<a><div>");
+        }
+    } catch{
+        res.send("Internal server error");
+    }
+});
+
+
+server.listen(3000, function(){
+    console.log("server is listening on port: 3000");
+    console.log('success. ')
+
+});*/
